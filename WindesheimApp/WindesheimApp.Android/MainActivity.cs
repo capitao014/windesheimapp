8c5c317214fef099bcddc0139ddf456be838523d
Diego Silva <diegonunescabral@hotmail.com>
2017-11-28 22:23:57 +0100

    First commit

---
+++ WindesheimApp/WindesheimApp.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using WindesheimApp.Helpers;
+
+namespace WindesheimApp.Droid
+{
+    [Activity(Label = "WindesheimApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+
+            
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            ThreadingHelper.InitializeFromUIThread();
+            CurrentPlatform.Init();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+           
+            
+
+        }
+    }
+}
+
